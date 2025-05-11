6180ed9826cbe1e7f572adcbd8c32910fe6da1b0
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-05-10 18:55:08 -0600

    Add JS to handle hiding the nav bar better on mobile.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,23 @@
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const nav = document.querySelector('nav');
 
-hamButton.addEventListener("click", () => {
-navigation.classList.toggle("open");
-hamButton.classList.toggle("open");
+hamButton.addEventListener('click', () => {
+navigation.classList.toggle('open');
+hamButton.classList.toggle('open');
+nav.classList.toggle('not-visible');
 });
+
+function checkViewportWidth() {
+    const viewportWidth = window.innerWidth;
+    if (viewportWidth < 700) {
+        nav.classList.add('not-visible');
+    }
+    else {
+        nav.classList.remove('not-visible');
+    }
+}
+
+window.addEventListener('resize', checkViewportWidth);
+
+document.addEventListener('DOMContentLoaded', checkViewportWidth);
