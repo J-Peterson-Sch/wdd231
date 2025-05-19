d5940d9b24d0123867182e740331b141ca187c32
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-05-19 11:42:07 -0600

    Saving before trying animated nav.

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,26 +1,32 @@
 const hamButton = document.querySelector('#menu');
 const modeButton = document.querySelector('#mode');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('.nav-link-list');
+const navContainer = document.querySelector('#nav-container')
 const nav = document.querySelector('nav');
 
 hamButton.addEventListener('click', () => {
-navigation.classList.toggle('open');
-hamButton.classList.toggle('open');
-nav.classList.toggle('not-visible');
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    navContainer.classList.toggle('open');
+    // if (window.innerWidth < 700) {
+    //     nav.classList.toggle('not-visible');
+    // } else {
+    //     nav.classList.remove('not-visible');
+    // }
 });
 
 modeButton.addEventListener('click', () => {
-    
+
 });
 
 function checkViewportWidth() {
     const viewportWidth = window.innerWidth;
-    if (viewportWidth < 700) {
-        nav.classList.add('not-visible');
-    }
-    else {
-        nav.classList.remove('not-visible');
-    }
+    // if (viewportWidth < 700) {
+    //     nav.classList.add('not-visible');
+    // }
+    // else {
+    //     nav.classList.remove('not-visible');
+    // }
 }
 
 window.addEventListener('resize', checkViewportWidth);
