4a5ca2d7878c71ff425096e4526a4cbdd66b1618
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-05-30 22:58:28 -0700

    Got the prototype working.

--- chamber/scripts/last-visit.js
+++ chamber/scripts/last-visit.js
@@ -1,13 +1,22 @@
 const msToDays = 86400000;
 const theDateToday = Date.now();
-const lastVisit = 1738660293454;
 
-const todayElement = document.getElementById('today');
-todayElement.textContent = theDateToday;
-console.log(theDateToday);
 
-const lastVisitElement = document.getElementById('last-visit');
-lastVisitElement.textContent = lastVisit;
+// ------------------------------------------------------------------------
 
-const diffElement = document.getElementById('days-since-last-visit');
-diffElement.textContent = `${Math.floor((theDateToday - lastVisit) / msToDays)} days since last visit`;
+let lastVisit = localStorage.getItem('lastVisitedDate') || false;
+
+if (lastVisit){
+    const todayElement = document.getElementById('today');
+    todayElement.textContent = theDateToday;
+    console.log(theDateToday);
+
+    const lastVisitElement = document.getElementById('last-visit');
+    lastVisitElement.textContent = lastVisit;
+
+    const diffElement = document.getElementById('days-since-last-visit');
+    diffElement.textContent = `${Math.floor((theDateToday - lastVisit) / msToDays)} days since last visit`;
+} else {
+    localStorage.lastVisitedDate = theDateToday
+}
+
