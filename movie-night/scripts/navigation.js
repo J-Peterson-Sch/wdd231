797220f85bf77473ca49e8f3b61a24caf524c559
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-06-10 19:40:58 -0600

    Nav work.

--- movie-night/scripts/navigation.js
+++ movie-night/scripts/navigation.js
@@ -1,4 +1,5 @@
 const hamButton = document.querySelector('#menu');
+const modeButton = document.querySelector('#mode');
 const navigation = document.querySelector('.nav-link-list');
 const navContainer = document.querySelector('#nav-container')
 const nav = document.querySelector('nav');
@@ -8,6 +9,10 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
     navContainer.classList.toggle('open');
+});
+
+modeButton.addEventListener('click', () => {
+
 });
 
 function checkViewportWidth() {
